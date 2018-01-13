5c6aaa96440da292b8ac34cf31346b2142e2bcc4
Guest User <Guest@Epicodus-3C.local>
2018-01-12 16:55:47 -0800

    add stylizing, play with maps but not use

--- dev/js/main-logic.js
+++ dev/js/main-logic.js
@@ -3,6 +3,7 @@ var apiKey = require('./../../.env').apiKey;
 export function findDoctor(symptoms, compile){
 
   const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&skip=0&limit=10&user_key=${apiKey}`);
+
   // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&user_key=${apiKey}`)
 
 // once you have api link, pull...
