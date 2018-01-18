8a09ff30ae5969ad76bab153e5d81598bbcab965
Guest User <Guest@Epicodus-3A.local>
2018-01-17 17:38:56 -0800

    change from targeting objects to arrays in main-logic.js info.forEach loop

--- dev/js/main-logic.js
+++ dev/js/main-logic.js
@@ -4,7 +4,6 @@ export function findDoctor(symptoms, compile){
 
   const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&skip=0&limit=10&user_key=${apiKey}`);
 
-  // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&user_key=${apiKey}`)
 
 // once you have api link, pull...
   xhr.done(function(responses){
@@ -14,24 +13,24 @@ export function findDoctor(symptoms, compile){
     const phoneNumbers = [];
     const websites = [];
     const availabilities = [];
-    let response = responses.data;
-    console.log("xhr is " + xhr);
-    console.log(response);
+    const info = responses.data;
 
-    responses.data.forEach(function(response){
-      names.push(response.data.practices.name);
-      addresses.push(response.data.practices.visit_address.street + " " + response.data.practices.visit_address.street2 + " " + response.data.practices.visit_address.city + " " + response.data.practices.visit_address.state + " " + response.data.practices.visit_address.zip);
-      //alternate addresses
-        //addresses.push(response.data.practices.visit_address.street + " " + response.data.practices.visit_address.street2 + " " + response.data.practices.visit_address.city + " " + response.data.practices.visit_address.state + " " + response.data.practices.visit_address.zip)
-      phoneNumbers.push(response.data.practices.phones.number);
-      websites.push(response.data.practices.website);
-      availabilities.push(response.data.practices.accepts_new_patients);
+console.log(responses);
+
+    info.forEach(function(info){
+      names.push(info.profile.first_name + " " + info.profile.last_name);
+      addresses.push(info.practices[0].visit_address.street + " " + info.practices[0].visit_address.street2 + " " + info.practices[0].visit_address.city + " " + info.practices[0].visit_address.state + " " + info.practices[0].visit_address.zip);
+      // HAVE LOOP SO IF NO STREET2 THEN IT'S ""
+      phoneNumbers.push(info.practices[0].phones[0].number);
+      // websites.push(info.practices[0].website);
+      availabilities.push(info.practices[0].accepts_new_patients);
     });
+
     console.log(names);
-    // console.log(addresses);
-    // console.log(phoneNumbers);
+    console.log(addresses);
+    console.log(phoneNumbers);
     // console.log(websites);
-    // console.log(availabilities);
+    console.log(availabilities);
   });
 
   xhr.fail(function(){
